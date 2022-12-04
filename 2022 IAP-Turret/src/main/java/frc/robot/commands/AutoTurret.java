2b28d7cccf4c02ca104a96c5b6ce2dd83043c53a
ZakBMorgan <zakbmorgan@gmail.com>
2022-12-03 19:27:12 -0800

    AutoTurret
    more comments

--- 2022 IAP-Turret/src/main/java/frc/robot/commands/AutoTurret.java
+++ 2022 IAP-Turret/src/main/java/frc/robot/commands/AutoTurret.java
@@ -11,47 +11,49 @@ import frc.robot.subsystems.Turret;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
 public class AutoTurret extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
 
   public final limelight lime;
   public final Turret turret;
   public PIDController pid;
+
+  // The constant where the limit switch returns as closed
   public int limitSwitchClosed = 1;
+
+  // The constant where the limit switch returns as opened
   public int limitSwitchOpen = 0;
+
+  // When the limit swith is manually toggled 
   public boolean manualToggle = true;
   public boolean directionToggle;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
+ 
   public AutoTurret(limelight lime, Turret turret) {
-    // Use addRequirements() here to declare subsystem dependencies.
+
     this.turret = turret;
     this.lime = lime;
 
-     /*
-    * P is the Porportional constant
-    * It will correct the error depending on how big the amount of error is
+   // P is the Proportional constant 
+    /* It will correct the error depending on how big the amount of error is:
     * Small amount of error = low correction, High = larger correction
     */
     double kP = 0.0;
-    /*
-    * I is the Integral constant
-    * It adds up all the past errors to help remove constant errors, because
-    * no matter how small the constant error is, the sum will be significant enough 
+
+    // I is the Integral constant
+    /* It adds up all the past errors to help remove constant errors because
+    * no matter how small the constant error, the sum will be significant enough 
     * to adjust the controller output as needed
     */
     double kI = 0.0;
-    /*
-    * D is the Derivative constant
-    * It will predict the amount of error in the future, because it examines
-    * the slope of the change in error
+
+    // D is the derivative constant
+    /* It will predict the amount of error in the future because it examines
+    * the slope of the change in error 
     */
     double kD = 0.0;
+
+    // PID is used to make small adjustments to achieve more precise movement
     pid = new PIDController(kP, kI, kD); 
 
   }
@@ -68,39 +70,69 @@ public class AutoTurret extends CommandBase {
   @Override
   public void execute() {
 
+    // 
     if(RobotContainer.getJoy1().getTriggerReleased()){
       manualToggle = !manualToggle;
     }
+
     if(manualToggle == true){
-      turret.spin(-0.2*RobotContainer.getJoy1().getX()); //The joystick returns -1 so we use -0.2
+
+      // The joystick returns -1 so we use -0.2
+      turret.spin(-0.2*RobotContainer.getJoy1().getX()); 
     }
+
     if(!manualToggle == true){
 
+      // This is an if statement when there is a valid target (tv == 1) and the reverse 
+      //or forward limit switch is not closed
+      // This is just a placeholder for safety
       if(lime.get_tv() == 1 && !(turret.getCCW_Reverse_LimitSw() == limitSwitchClosed | turret.getCW_Forward_LimitSw() == limitSwitchClosed)){
+
+        // Use the pid to precisely turn the chassis horizontally at a calculated speed
         double speed = pid.calculate(lime.get_tx());
-        turret.spin(speed); //This should spin clockwise
+
+        // This should spin clockwise if there is a positive x position
+        turret.spin(speed); 
+
       }
 
+      // If the limelight has no valid targets
       else if(lime.get_tv() == 0){
 
         if(turret.getCCW_Reverse_LimitSw() == limitSwitchClosed){
+
+          // It is true when the reverse limit switch is hit
+          // Will go reverse in direction, so clockwise
           directionToggle = true;
+
         }
 
         if(turret.getCW_Forward_LimitSw() == limitSwitchClosed){
+
+          // It is true when the forward limit switch is hit
+          // Will go forward in direction, so counter clockwise
           directionToggle = false;
+
         }
 
         if(directionToggle == true){
-          turret.spin(0.3); //Will go CW if the reverse limit switch is hit
+
+          // Will go clockwise if the reverse limit switch is hit
+          turret.spin(0.3); 
+
         }
 
         if(!directionToggle == true){
-          turret.spin(-0.3); //Will go CCW if the forward limit switch it hit
+          // Will go counter clockwise if the forward limit switch it hit
+          turret.spin(-0.3); 
+
         }
 
       } else {
-        turret.spin(0); //Will disable motor
+
+        // Will disable motor if all condition above aren't met
+        turret.spin(0); 
+
       }
     }
   }
@@ -114,6 +146,8 @@ public class AutoTurret extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+
     return false;
+
   }
 }
